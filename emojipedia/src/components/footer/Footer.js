6cf27903411c3bc6371bfb3d3e3fa681a88eb950
Suzanne Zomer <suzannezomer@gmail.com>
2023-02-23 19:35:11 +0100

    Finish footer component in emoji

--- emojipedia/src/components/footer/Footer.js
+++ emojipedia/src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import './footer.scss';
 
 function Footer() {
-  return <div className='Footer'>Footer</div>;
+  return <div className='Footer'>EmojiPedia &copy; All Rights Reserved.</div>;
 }
 
 export default Footer;
